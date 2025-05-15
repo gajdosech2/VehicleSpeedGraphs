920ef3b61cf5cb4f562ade3c9ca156b8be7ec822
gajdosech2 <l.gajdosech@gmail.com>
2025-05-15 14:31:03 +0200

    new

--- backup/graphs.py
+++ backup/graphs.py
@@ -4,19 +4,19 @@ from itertools import cycle
 
 # The provided configuration results dictionary
 CONFIG_RESULTS = {
-    "nano, 1int8, titanV, 640x360": (355, 0.71, 0.55, 91.63, 0.84),
-    "nano, 2fp16, titanV, 640x360": (267, 0.80, 0.64, 92.41, 0.93),
-    "nano, 3fp32, titanV, 640x360": (221, 0.80, 0.64, 92.42, 0.93),
+    "nano, 1int8, titanV, 640x360": (355, 0.71, 0.55, 91.63, 83.95),
+    "nano, 2fp16, titanV, 640x360": (267, 0.80, 0.64, 92.41, 92.58),
+    "nano, 3fp32, titanV, 640x360": (221, 0.80, 0.64, 92.42, 92.57),
 
-    #"small, 1int8, titanV, 640x360": (302, 0.80, 0.62, 96.24, 85.22),
-    #"small, 2fp16, titanV, 640x360": (205, 0.77, 0.58, 93.41, 92.48),
-    #"small, 3fp32, titanV, 640x360": (142, 0.77, 0.59, 93.40, 92.48),
+    "small, 1int8, titanV, 640x360": (302, 0.80, 0.62, 96.24, 85.22),
+    "small, 2fp16, titanV, 640x360": (205, 0.77, 0.58, 93.41, 92.48),
+    "small, 3fp32, titanV, 640x360": (142, 0.77, 0.59, 93.40, 92.48),
 
-    #"medium, 2fp16, titanV, 640x360": (174, 0.86, 0.69, 93.15, 90.96),
-    #"medium, 3fp32, titanV, 640x360": (120, 0.86, 0.69, 93.13, 90.95),
+    "medium, 2fp16, titanV, 640x360": (174, 0.86, 0.69, 93.15, 90.96),
+    "medium, 3fp32, titanV, 640x360": (120, 0.86, 0.69, 93.13, 90.95),
 
-    #"large, 2fp16, titanV, 640x360": (159, 0.84, 0.66, 92.72, 90.94),
-    #"large, 3fp32, titanV, 640x360": (95, 0.84, 0.66, 92.75, 90.94),
+    "large, 2fp16, titanV, 640x360": (159, 0.84, 0.66, 92.72, 90.94),
+    "large, 3fp32, titanV, 640x360": (95, 0.84, 0.66, 92.75, 90.94),
 }
 
 def plot_results(fixed, shapes, colors, hw):
@@ -33,7 +33,7 @@ def plot_results(fixed, shapes, colors, hw):
     print(len(color_values))
 
     for fixed_val in fixed_values:
-        plt.figure(figsize=(11, 4))
+        plt.figure(figsize=(11, 5))
         current_markers = {shape: next(markers) for shape in shape_values}
         current_colors = {color: colormap((i+1) / len(color_values)) for i, color in enumerate(color_values)}
 
@@ -47,24 +47,20 @@ def plot_results(fixed, shapes, colors, hw):
                 shape = key_parts[{'dtype': 1, 'model_size': 0, 'input_size': 3, 'gpu': 2}[shapes]]
                 color = key_parts[{'dtype': 1, 'model_size': 0, 'input_size': 3, 'gpu': 2}[colors]]
 
-                plt.scatter(fps, recall, label=f"{shape:<6} {color:<6}", 
-                            color='black', marker='o', s=400) #current_markers[shape] current_colors[color]
+                plt.scatter(fps, mean_error, label=f"{shape:<6} {color:<6}", 
+                            color=current_colors[color], marker='o', s=400) #current_markers[shape]
 
         #plt.gca().invert_xaxis()
+        plt.gca().invert_yaxis()
         #plt.title(f"Graph for fixed {fixed}: {fixed_val} (HW: {hw})", fontsize=16)
-        #plt.ylim(0.68, 0.82)
-        plt.ylim(0.82, 0.94)
-        plt.xlim(205, 375)
-        #plt.gca().invert_yaxis()
         plt.xlabel(r"$\rightarrow$ FPS $\rightarrow$", fontsize=26)
         plt.ylabel("$\leftarrow$ Speed Error $\leftarrow$", fontsize=26)
-        plt.ylabel(r"$\rightarrow$ Recall $\rightarrow$", fontsize=26)
-        #plt.legend(title=f"Marker Model Size dType", fontsize=14, title_fontsize=12, loc='center left', bbox_to_anchor=(1, 0.5), prop={'family': 'monospace', 'size': 17})
+        plt.legend(title=f"Marker Model Size dType", fontsize=14, title_fontsize=12, loc='center left', bbox_to_anchor=(1, 0.5), prop={'family': 'monospace', 'size': 17})
         plt.grid(True, linestyle='--', alpha=0.7)
         plt.yticks(fontsize=18)
         plt.xticks(fontsize=19)
-        #plt.show()
-        plt.savefig(f'graph{fixed_val}.pdf', bbox_inches='tight')
+        plt.show()
+        #plt.savefig(f'graph{fixed_val}.pdf', bbox_inches='tight')
         #break
 
 # Example usage
--- boxplot.py
+++ boxplot.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
+import matplotlib.patches as mpatches
+import matplotlib.cm as cm
 
 # Dummy data: Replace these with your actual speed errors (one array per system)
 np.random.seed(42)  # For reproducibility
@@ -11,33 +13,50 @@ system_4 = np.random.normal(loc=1.1, scale=0.25, size=100)
 system_5 = np.random.normal(loc=0.85, scale=0.15, size=100)
 system_6 = np.random.normal(loc=0.95, scale=0.12, size=100)
 
-system_1 = np.loadtxt('yolov6_nano_23_b32_fp32_titan_errors_abs.txt', dtype=float).tolist()
-system_2 = np.loadtxt('yolov6_distill_nano_23_b32_fp32_titan_errors_abs.txt', dtype=float).tolist()
-system_3 = np.loadtxt('yolov6_small_23_b32_fp32_titan_errors_abs.txt', dtype=float).tolist()
-system_4 = np.loadtxt('yolov6_small_distill_23_fp16_fp32_titan_errors_abs.txt', dtype=float).tolist()
-system_5 = np.loadtxt('yolov6_medium_23_b32_fp32_titan_errors_abs.txt', dtype=float).tolist()
-system_6 = np.loadtxt('yolov6_large_23_b32_fp32_titan_errors_abs.txt', dtype=float).tolist()
+system_1 = np.loadtxt('SochorCVIU_Edgelets_ManualScale_errors_abs.txt', dtype=float).tolist()
+system_2 = np.loadtxt('Transform3D_960_540_VP2VP3_errors_abs.txt', dtype=float).tolist()
+system_3 = np.loadtxt('yolov6_nano_b32_640_352_fp32_titan_errors_abs.txt', dtype=float).tolist()
+system_4 = np.loadtxt('yolov6_nano_b1_640_352_int8_jupiter_errors_abs.txt', dtype=float).tolist()
+system_5 = np.loadtxt('yolov6_small_b32_640_352_fp32_titan_errors_abs.txt', dtype=float).tolist()
+system_6 = np.loadtxt('yolov6_small_b1_640_352_int8_jupiter_errors_abs.txt', dtype=float).tolist()
 
 # Combine all systems into a list for boxplotting
 data = [system_1, system_2, system_3, system_4, system_5, system_6]
-labels = ['Nano', 'Nano Distill', 'Small', 'Small Distill', 'Medium', 'Large']
+labels = ['Sochor\nManual', 'Transform3D\n960 x 540', 'Nano\n640 x 360\nFP32', 'Nano\n640 x 360\nINT8', 'Small\n640 x 360\nFP32', 'Small\n640 x 360\nINT8']
 
 # Plot
-plt.figure(figsize=(10, 6))
-plt.boxplot(data, labels=labels, patch_artist=True, showmeans=True, showfliers=False,
-            boxprops=dict(facecolor='lightblue', color='lightblue'),
-            medianprops=dict(color='red'),
-            meanprops=dict(marker='o', markerfacecolor='green', markersize=5),
+plt.figure(figsize=(6, 5))
+box = plt.boxplot(data, labels=labels, patch_artist=True, showmeans=True, showfliers=False,
+            boxprops=dict(facecolor='lightgray', color='lightgray'),
+            medianprops=dict(color='gray', linewidth=2.5),
+            meanprops=dict(marker='o', markerfacecolor='gray', markeredgecolor='gray', markersize=5),
             whiskerprops=dict(color='gray'),
             capprops=dict(color='gray'),
             flierprops=dict(marker='x', markerfacecolor='gray', markersize=5))
 
+
+colors = ['lightblue', 'lightgreen', 'lightcoral', 'wheat', 'plum', 'red']
+colormap = cm.get_cmap('YlOrRd') 
+colors = ['lightblue', 'lightblue', colormap(3 / 3), colormap(1 / 3), colormap(3 / 3), colormap(1 / 3)]
+
+#handles = []
+#for patch, color, label in zip(box['boxes'], colors, labels):
+#    patch.set_facecolor(color)
+#    handles.append(mpatches.Patch(color=color, label=label))
+
+#plt.xticks([])
+
+#plt.legend(handles=handles, loc='center left', bbox_to_anchor=(1.02, 0.5), borderaxespad=0)
+
 ax = plt.gca()
-ax.yaxis.set_major_locator(MultipleLocator(0.1)) 
+ax.yaxis.set_major_locator(MultipleLocator(0.2)) 
 ax.grid(which='major', axis='y', linestyle='--', alpha=0.7)
 
+#plt.xticks(rotation=-60)
+
 plt.ylabel('Speed Error (km/h)')
-plt.title('Boxplot of Speed Estimation Errors')
+#plt.title('Boxplot of Speed Estimation Errors')
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 plt.tight_layout()
+plt.savefig(f'boxplot.pdf', bbox_inches='tight')
 plt.show()
